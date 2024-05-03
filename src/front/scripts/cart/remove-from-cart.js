e0f02356b3749292bb6d1c358cec2d5827a3056a
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2024-05-03 19:04:09 +0200

    edited remove-from-cart, added new totalprice modification when removing item from cart

--- src/front/scripts/cart/remove-from-cart.js
+++ src/front/scripts/cart/remove-from-cart.js
@@ -4,18 +4,28 @@ const removeButtons = document.querySelectorAll('.remove-button');
 
 for (const button of removeButtons) {
   button.addEventListener('click', (e) => {
+    //Getting the product id and the subtotal price of the item to be removed
     const productId = e.target.dataset.productid;
+    const subtotal = parseFloat(
+      button.closest('.cart-item-li').querySelector('.item-subtotal-price').innerHTML
+    ).toFixed(2);
+
+    //Getting the total price of the cart
+    const totalPriceElement = document.querySelector('.total-price');
+    const totalPrice = parseFloat(totalPriceElement.innerHTML).toFixed(2);
 
     try {
+      //Getting the cookie, removing the product and saving the new cart
       const cart = getCookie('cart');
 
       const newCart = cart.filter((item) => item.id !== productId);
 
       setCookie('cart', JSON.stringify(newCart));
-
+      // Hiding the item from the cart and updating the total price
       button.closest('.cart-item-li').style.display = 'none';
+      totalPriceElement.innerHTML = (totalPrice - subtotal).toFixed(2);
     } catch (e) {
-      console.log('An error ocurred');
+      console.log('An error occurred while removing the item from the cart.');
       return;
     }
   });
--- src/front/scripts/cookies.js
+++ src/front/scripts/cookies.js
@@ -5,8 +5,11 @@
  * @param {number} [exdays=30] - The number of days until the cookie expires (default is 30 days).
  */
 export function setCookie(cname, cvalue, exdays = 30) {
+  // Create a new date object
   const d = new Date();
+  // Set the expiration date
   d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  // Set the cookie
   let expires = 'expires=' + d.toUTCString();
   document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
@@ -17,13 +20,18 @@ export function setCookie(cname, cvalue, exdays = 30) {
  * @returns {any} The value of the cookie in JSON format, or null if the cookie does not exist.
  */
 export function getCookie(cname) {
+  // Get the cookie by its name
   let name = cname + '=';
   let ca = document.cookie.split(';');
+  // Loop through the cookies
   for (let i = 0; i < ca.length; i++) {
+    // Get the current cookie
     let c = ca[i];
+    // Remove leading spaces
     while (c.charAt(0) == ' ') {
       c = c.substring(1);
     }
+    // Check if the cookie is the one we are looking for
     if (c.indexOf(name) == 0) {
       const decodedCookie = decodeURIComponent(c.substring(name.length, c.length));
       return JSON.parse(decodedCookie);
